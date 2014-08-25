c96baaf813d65fb66ac57c02636859e7e4b517cf
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-25 02:17:37 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,15 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
-var fs = require('fs');
 var isGzip = require('../');
 var path = require('path');
+var readChunk = require('read-chunk');
+var test = require('ava');
 
-describe('isGzip()', function () {
-    function check(file) {
-        return isGzip(fs.readFileSync(file));
-    }
+test('should detect GZIP from buffer', function (t) {
+    t.plan(2);
 
-    it('should detect GZIP from buffer', function (cb) {
-        assert(check(path.join(__dirname, 'fixtures/test.tar.gz')));
-        cb();
-    });
-
-    it('should detect GZIP from Uint8Array', function () {
-        var buf = new Uint8Array([31, 139, 8, 0]);
-        assert(isGzip(buf));
+    readChunk(path.join(__dirname, 'fixtures/test.tar.gz'), 0, 4, function (err, buf) {
+        t.assert(!err);
+        t.assert(isGzip(buf));
     });
 });
